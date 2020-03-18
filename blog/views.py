dff6374828d3de84a1ce5da72f6d60ab90c3c119
Katarzyna Chelstowska <k.chelstowska@gmail.com>
2020-03-18 19:11:59 +0100

    blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog_post
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blog_posts = Blog_post.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blog_post':blog_posts})
