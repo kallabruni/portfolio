c9b9cb0a4e74b22e370c54321b605ad1e971e72c
Katarzyna Chelstowska <k.chelstowska@gmail.com>
2020-03-19 19:56:52 +0100

    static file

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Blog_post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     date = models.DateField()
-    description = models.CharField(max_length=250)
+    description = models.TextField()
