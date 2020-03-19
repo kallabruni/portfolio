031da00d631dfbc030235248a7deb331db3fafac
Katarzyna Chelstowska <k.chelstowska@gmail.com>
2020-03-19 19:56:52 +0100

    static file

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Blog_post
 
 def all_blogs(request):
-    blog_posts = Blog_post.objects.all()
-    return render(request, 'blog/all_blogs.html', {'blog_post':blog_posts})
+    blog_posts = Blog_post.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blog_posts':blog_posts})
