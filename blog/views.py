df02492871449c3f53ae0b19cc16ac512c6aa21f
Katarzyna Chelstowska <k.chelstowska@gmail.com>
2020-03-21 23:56:37 +0100

    singel post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog_post
+
 
 def all_blogs(request):
     blog_posts = Blog_post.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blog_posts':blog_posts})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog_post, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
